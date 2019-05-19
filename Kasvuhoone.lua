95d1e5cfedf44833f5f9ef6dd0c0e218e8b433dc
rainerse <50597933+rainerse@users.noreply.github.com>
2019-05-19 20:09:30 +0300

    Update Kasvuhoone.lua

--- Kasvuhoone.lua
+++ Kasvuhoone.lua
@@ -13,22 +13,23 @@ local avamine               = "open";    -- avamine
 local sulgemine             = "close";   -- sulgemine
 local temperatuur_tegelik   = (tonumber(fibaro:getValue(temp_andur, "value")));
 local akna_olek             = (tonumber(fibaro:getValue(seade, "value")));
-print('<font color=lightblue> # 16 Käivitan stseeni');
-print('<font color=lightblue> # 17 Kasvuhoone temperatuur: '.. temperatuur_tegelik .. '. Akna olek: ' .. akna_olek);
+local startSource = fibaro:getSourceTrigger();
+print('<font color=lightblue> # 17 Käivitan stseeni');
+print('<font color=lightblue> # 18 Kasvuhoone temperatuur: '.. temperatuur_tegelik .. '. Akna olek: ' .. akna_olek);
 if(temperatuur_tegelik) < (temperatuur_sulgemine) then -------------------------------------------------------------------------
-    print('<font color=lightblue> # 19 Temperatuur on alla ' .. temperatuur_sulgemine ..'C. Kontrollin kas aken on lahti.');  --
+    print('<font color=lightblue> # 20 Temperatuur on alla ' .. temperatuur_sulgemine ..'C. Kontrollin kas aken on lahti.');  --
     if (akna_olek > 1) then -------------------------------------------------------                                          --
-       print('<font color=pink> # 21 aken on lahti, panen akna kinni');           --                                          --
+       print('<font color=pink> # 22 aken on lahti, panen akna kinni');           --                                          --
        fibaro:call(seade, sulgemine);                                             --     --sulgemine                          --
     else                                                                          --                                          --
-       print('<font color=lightblue> # 24 Aken on kinni, ei tee midagi');         --                                          --
+       print('<font color=lightblue> # 25 Aken on kinni, ei tee midagi');         --                                          --
     end ----------------------------------------------------------------------------                                          --
 elseif (temperatuur_tegelik) > (temperatuur_avamine) then                                                                    --
-    print('<font color=lightblue> # 27 Temperatuur on üle ' .. temperatuur_avamine .. 'C, kontrollin kas aken on kinni');     --
+    print('<font color=lightblue> # 28 Temperatuur on üle ' .. temperatuur_avamine .. 'C, kontrollin kas aken on kinni');     --
     if (akna_olek) < 1 then ------------------------------------------------------                                          --
-       print('<font color=lightgreen> # 29 Aken on kinni, avan akna.');           --                                          --
+       print('<font color=lightgreen> # 30 Aken on kinni, avan akna.');           --                                          --
        fibaro:call(seade, avamine);                                               --     --avamine                            --
     else                                                                          --                                          --
-    	print('<font color=lightblue> # 32 Aken on lahti, ei tee midagi.');       --                                          --
+    	print('<font color=lightblue> # 33 Aken on lahti, ei tee midagi.');       --                                          --
     end ----------------------------------------------------------------------------                                          --
 end ----------------------------------------------------------------------------------------------------------------------------
