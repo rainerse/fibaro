8d4b2c3c43418c15e5f4b037a8f8322dc53f8929
rainerse <50597933+rainerse@users.noreply.github.com>
2019-05-20 09:55:59 +0300

    Update Kasvuhoone.lua

--- Kasvuhoone.lua
+++ Kasvuhoone.lua
@@ -5,31 +5,31 @@
 %% events
 %% globals
 --]]
-local temp_andur            = 2166;      -- temperatuuriandur
-local seade                 = 2165;      -- seade mida lülitatakse
-local temperatuur_avamine   = 30;        --C avamistemperatuur
-local temperatuur_sulgemine = 20;        --C sulgemistemperatuur
-local avamine               = "open";    -- avamine
-local sulgemine             = "close";   -- sulgemine
-local temperatuur_tegelik   = (tonumber(fibaro:getValue(temp_andur, "value")));
-local akna_olek             = (tonumber(fibaro:getValue(seade, "value")));
-local startSource = fibaro:getSourceTrigger();
-print('<font color=lightblue> # 17 Käivitan stseeni');
-print('<font color=lightblue> # 18 Kasvuhoone temperatuur: '.. temperatuur_tegelik .. '. Akna olek: ' .. akna_olek);
-if(temperatuur_tegelik) < (temperatuur_sulgemine) then -------------------------------------------------------------------------
-    print('<font color=lightblue> # 20 Temperatuur on alla ' .. temperatuur_sulgemine ..'C. Kontrollin kas aken on lahti.');  --
-    if (akna_olek > 1) then -------------------------------------------------------                                          --
-       print('<font color=pink> # 22 aken on lahti, panen akna kinni');           --                                          --
-       fibaro:call(seade, sulgemine);                                             --     --sulgemine                          --
-    else                                                                          --                                          --
-       print('<font color=lightblue> # 25 Aken on kinni, ei tee midagi');         --                                          --
-    end ----------------------------------------------------------------------------                                          --
-elseif (temperatuur_tegelik) > (temperatuur_avamine) then                                                                    --
-    print('<font color=lightblue> # 28 Temperatuur on üle ' .. temperatuur_avamine .. 'C, kontrollin kas aken on kinni');     --
-    if (akna_olek) < 1 then ------------------------------------------------------                                          --
-       print('<font color=lightgreen> # 30 Aken on kinni, avan akna.');           --                                          --
-       fibaro:call(seade, avamine);                                               --     --avamine                            --
-    else                                                                          --                                          --
-    	print('<font color=lightblue> # 33 Aken on lahti, ei tee midagi.');       --                                          --
-    end ----------------------------------------------------------------------------                                          --
-end ----------------------------------------------------------------------------------------------------------------------------
+local temp_sensor            = 2166;      -- temperature sensor ID
+local window                 = 2165;      -- Window device ID
+local temp_open              = 30;        --C Opening temperature
+local temp_close             = 20;        --C closing temperature
+local open                   = "open";    -- opening command
+local close                  = "close";   -- closing command
+local temp_actual            = (tonumber(fibaro:getValue(temp_sensor, "value")));  -- actual temperature 
+local window_status          = (tonumber(fibaro:getValue(window, "value")));       -- actual window status
+
+print('<font color=lightblue> # 17 Starting scene');
+print('<font color=lightblue> # 18 Actual temperature is: '.. temp_actual .. '. Window status: ' .. window_status);
+if(temp_actual) < (temp_close) then -------------------------------------------------------------------------------
+    print('<font color=lightblue> # 20 Temperature less ' .. temp_close ..'C. checking device status.');         --
+    if (window_status > 1) then -------------------------------------------------------                          --
+       print('<font color=pink> # 22 Window is open. Closing the window');           --                          --
+       fibaro:call(window, close);                                                   --     -- closing           --
+    else                                                                             --                          --
+       print('<font color=lightblue> # 25 Window is closed. Don't do anything');     --                          --
+    end -------------------------------------------------------------------------------                          --
+elseif (temp_actual) > (temp_open) then                                                                          --
+    print('<font color=lightblue> # 28 Temperature is over ' .. temp_open .. 'C, checking is the window open');  --
+    if (window_status) < 1 then -------------------------------------------------------                          --
+       print('<font color=lightgreen> # 30 Window is closed, opening the window.');  --                          --
+       fibaro:call(window, open);                                                    --     -- opening           --
+    else                                                                             --                          --
+    	print('<font color=lightblue> # 33 Window is open, don't do anything.');     --                          --
+    end -------------------------------------------------------------------------------                          --
+end ---------------------------------------------------------------------------------------------------------------
