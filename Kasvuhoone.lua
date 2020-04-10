a3e443e8c6fd1b310a136ed8d55d6aeb78eab63d
rainerse <50597933+rainerse@users.noreply.github.com>
2020-04-10 23:08:13 +0300

    Update Kasvuhoone.lua

--- Kasvuhoone.lua
+++ Kasvuhoone.lua
@@ -1,35 +1,44 @@
 --[[
 %% properties
-2166 value
+117 value
 %% weather
 %% events
 %% globals
 --]]
-local temp_sensor            = 2166;      -- temperature sensor ID
-local window                 = 2165;      -- Window device ID
-local temp_open              = 30;        --C Opening temperature
-local temp_close             = 20;        --C closing temperature
-local open                   = "open";    -- opening command
-local close                  = "close";   -- closing command
-local temp_actual            = (tonumber(fibaro:getValue(temp_sensor, "value")));  -- actual temperature 
-local window_status          = (tonumber(fibaro:getValue(window, "value")));       -- actual window status
-
-print('<font color=lightblue> # 17 Starting scene');
-print('<font color=lightblue> # 18 Actual temperature is: '.. temp_actual .. '. Window status: ' .. window_status);
-if(temp_actual) < (temp_close) then -------------------------------------------------------------------------------
-    print('<font color=lightblue> # 20 Temperature less ' .. temp_close ..'C. checking device status.');         --
-    if (window_status > 1) then -------------------------------------------------------                          --
-       print('<font color=pink> # 22 Window is open. Closing the window');           --                          --
-       fibaro:call(window, close);                                                   --     -- closing           --
-    else                                                                             --                          --
-       print('<font color=lightblue> # 25 Window is closed. Don't do anything');     --                          --
-    end -------------------------------------------------------------------------------                          --
-elseif (temp_actual) > (temp_open) then                                                                          --
-    print('<font color=lightblue> # 28 Temperature is over ' .. temp_open .. 'C, checking is the window open');  --
-    if (window_status) < 1 then -------------------------------------------------------                          --
-       print('<font color=lightgreen> # 30 Window is closed, opening the window.');  --                          --
-       fibaro:call(window, open);                                                    --     -- opening           --
-    else                                                                             --                          --
-    	print('<font color=lightblue> # 33 Window is open, don't do anything.');     --                          --
-    end -------------------------------------------------------------------------------                          --
-end ---------------------------------------------------------------------------------------------------------------
+local temp_andur = 117  	--temperatuuri andur 1347 value  minu temp andur on id117
+local seade = 112 			-- seade mida lülitatakse > 1346 value  minu relee on id 28
+local avamis_temp = 30		-- avamistemperatuur
+local sulgemis_temp = 20	-- sulgemistemperatuur
+local avamine = "turnOn"	-- "open"  avamine
+local sulgemine = "turnOff"	-- "close" sulgemine
+local temp_value = (tonumber(fibaro:getValue(temp_andur, "value")));
+local akna_olek = (tonumber(fibaro:getValue(seade, "value")));
+--local startSource = fibaro:getSourceTrigger();
+print("<font color='aqua'># Käivitan stseeni</font>");    
+print("<font color='lightgreen'># Temperatuur: " .. temp_value);
+print("<font color='lightgreen'># Akna olek:" .. akna_olek);
+-----------------------------------------------------------------------------------------------------------------------
+if temp_value <= sulgemis_temp then                                                                                  --
+    print("<font color='aqua'># temperatuur on alla " .. sulgemis_temp .." kraadi. Kontrollin kas aken on lahti.");  --
+    if ((tonumber(fibaro:getValue(seade, "value"))) == 1) then                                                       --
+       print("<font color='Salmon'># Aken on lahti, panen akna kinni");                                              --
+       fibaro:call(seade, sulgemine);                                                 --sulgemine                    --
+    else                                                                                                             --
+       print("<font color='aqua'># Aken on kinni, ei tee midagi");                                                   --
+    end                                                                                                              --
+end                                                                                                                  --
+-----------------------------------------------------------------------------------------------------------------------
+if temp_value >= avamis_temp then                                                                                    --
+    print("<font color='aqua'># Temperatuue on üle " .. avamis_temp .. " kraadi, kontrollin kas aken on kinni");     --
+    if akna_olek ==0 then                                                                                            --
+       print("<font color='Salmon'># Aken on kinni, avan akna.");                                                    --
+       fibaro:call(seade, avamine);                                                   --avamine                      --
+    else                                                                                                             --
+    	print("<font color='aqua'># Aken on lahti, ei tee midagi.");                                                 --
+    end                                                                                                              --
+end                                                                                                                  --
+-----------------------------------------------------------------------------------------------------------------------
+if temp_value < avamis_temp and temp_value > sulgemis_temp then                                                      --
+    print("<font color='aqua'># temperatuur on sulgemise ja avamise vahel. Ei tee midagi");                          --
+end                                                                                                                  --
+-----------------------------------------------------------------------------------------------------------------------
